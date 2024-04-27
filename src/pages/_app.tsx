376a4eb0117cfc17a12bb38354bfe06ec97b010e
Roberta Quiles <roberta@MacBook-Pro-de-Roberta.local>
2024-04-27 18:37:25 -0300

    Fix double quote

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import { Nunito } from "next/font/google";
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Nunito } from 'next/font/google';
 
-const nunito = Nunito({ subsets: ["latin"] });
+const nunito = Nunito({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
