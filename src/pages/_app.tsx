b0cb36af3fd11d0ec82a1305097bc8a934103e96
Roberta Quiles <roberta@MacBook-Pro-de-Roberta.local>
2024-04-27 20:31:17 -0300

    Add Layout component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Nunito } from 'next/font/google';
+import Layout from './components/Layout';
 
 const nunito = Nunito({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={nunito.className}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </main>
   );
 }
