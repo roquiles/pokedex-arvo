c1d7519395fca7a02546669b13b7621abf724128
Roberta Quiles <roberta@MacBook-Pro-de-Roberta.local>
2024-04-28 16:53:54 -0300

    Add default exports

---
+++ src/utils/index.ts
@@ -0,0 +1,4 @@
+export { default as getAttributesList } from './getAttributesList';
+export { default as getStatsList } from './getStatsList';
+export { default as pokemonInfoParser } from './pokemonInfoParser';
+export { default as validatePokemonId } from './validatePokemonId';
