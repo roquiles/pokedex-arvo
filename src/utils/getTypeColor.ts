cbe8390b9d19c6caa2ca60f228780895d33e9776
Roberta Quiles <roberta@MacBook-Pro-de-Roberta.local>
2024-04-29 20:13:06 -0300

    Create helper function to map type color

---
+++ src/utils/getTypeColor.ts
@@ -0,0 +1,28 @@
+const getTypeColor = (type: string) => {
+  const typeColors: { [key: string]: string } = {
+    bug: '#E3D985',
+    dark: '#D1D5DE',
+    dragon: '#FFA987',
+    electric: '#FFC100',
+    fairy: '#FBACBE',
+    fire: '#E76F51',
+    fighting: '#FF6B6B',
+    flying: '#A4BFEB',
+    ghost: '#BEB7A4',
+    grass: '#C8E087',
+    ground: '#CEA07E',
+    ice: '#77B6EA',
+    normal: '#D9D9D9',
+    poison: '#CFBCDF',
+    psychic: '#DFB2F4',
+    rock: '#B7B5B3',
+    shadow: '#546A76',
+    steel: '#D8DBE2',
+    unknown: '#C3C49E',
+    water: '#1CCAD8',
+  };
+
+  return typeColors[type] || '#FFF';
+};
+
+export default getTypeColor;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,4 @@ export { default as getAttributesList } from './getAttributesList';
 export { default as getStatsList } from './getStatsList';
 export { default as pokemonInfoParser } from './pokemonInfoParser';
 export { default as validatePokemonId } from './validatePokemonId';
+export { default as getTypeColor } from './getTypeColor';
