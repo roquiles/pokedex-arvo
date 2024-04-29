9a744a26df94ee12c35934d418c7d069a73579c8
Roberta Quiles <roberta@MacBook-Pro-de-Roberta.local>
2024-04-29 11:33:09 -0300

    Show error and loading state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import Head from 'next/head';
 import useFetchPokemon from '@/hooks/useFetchPokemon';
 import SearchBar from './components/SearchBar';
+import { Empty, LoaderSpinner, UpperArrow } from './components/Icons';
 
 const Home = () => {
-  const { fetchPokemon } = useFetchPokemon();
+  const { fetchPokemon, isLoading, error } = useFetchPokemon();
 
   return (
     <>
@@ -17,11 +18,26 @@ const Home = () => {
         <h1 className="text-gray-800 text-4xl font-bold pb-6">
           Welcome to Pokedéx!
         </h1>
-        <p className="text-gray-700 pb-6">
-          Enter a Pokémon&apos;s name or number in the search bar below to begin
-          your journey.
-        </p>
-        <SearchBar onSearch={fetchPokemon} />
+        <SearchBar onSearch={fetchPokemon} isLoadingSearch={isLoading} />
+
+        {isLoading && <LoaderSpinner />}
+
+        {!isLoading && !error && (
+          <>
+            <UpperArrow />
+            <p className="text-gray-700 pb-6">
+              Enter a Pokémon&apos;s name or number in the search bar above to
+              begin your journey.
+            </p>
+          </>
+        )}
+
+        {!isLoading && error && (
+          <>
+            <Empty />
+            <p>{error}</p>
+          </>
+        )}
       </div>
     </>
   );
