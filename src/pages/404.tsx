2c674f961d13ad7ca7bd1e0c6bbd2716047fb990
Roberta Quiles <roberta@MacBook-Pro-de-Roberta.local>
2024-04-30 11:38:36 -0300

    Add custom 404 error page

---
+++ src/pages/404.tsx
@@ -0,0 +1,7 @@
+const Custom404: React.FC = () => (
+  <div className="h-full flex justify-center items-center">
+    <h1 className="text-xl">404 - Page Not Found</h1>
+  </div>
+);
+
+export default Custom404;
