9e8b46c06f655ddd9d890121d7d5581c174e3d30
Roberta Quiles <roberta@MacBook-Pro-de-Roberta.local>
2024-04-29 23:13:35 -0300

    Adjust component style

--- src/pages/components/Pokemon/PokemonTypes.tsx
+++ src/pages/components/Pokemon/PokemonTypes.tsx
@@ -1,9 +1,9 @@
 import { getTypeColor } from '@/utils';
 
 const PokemonTypes: React.FC<{ types: string[] }> = ({ types }) => (
-  <div className="ml-4 text-start">
+  <div className="ml-4 mb-10 text-start">
     <h3 className="text-2xl text-gray-800 font-semibold mb-4">Types</h3>
-    {types.map((item) => (
+    {types?.map((item) => (
       <span
         style={{ backgroundColor: getTypeColor(item) }}
         className="px-6 py-1 rounded-full mr-2"
