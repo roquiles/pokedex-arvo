2dfec142fba1ba6918970b2a12cd35cc23b5f29b
Roberta Quiles <roberta@MacBook-Pro-de-Roberta.local>
2024-04-29 23:30:47 -0300

    Add page reload when clicking on header

--- src/pages/components/Header/index.tsx
+++ src/pages/components/Header/index.tsx
@@ -1,19 +1,23 @@
 import Image from 'next/image';
-import Link from 'next/link';
+import { useRouter } from 'next/router';
 
-const Header: React.FC = () => (
-  <header className="w-full bg-indigo-900 h-16 flex items-center justify-center shadow-xl">
-    <Link href="/">
-      <Image
-        src="/logo.png"
-        alt="Pokédex"
-        width="0"
-        height="0"
-        sizes="100vw"
-        className="w-auto h-14"
-      />
-    </Link>
-  </header>
-);
+const Header: React.FC = () => {
+  const router = useRouter();
+
+  return (
+    <header className="w-full bg-indigo-900 h-16 flex items-center justify-center shadow-xl">
+      <button type="button" onClick={() => router.reload()}>
+        <Image
+          src="/logo.png"
+          alt="Pokédex"
+          width="0"
+          height="0"
+          sizes="100vw"
+          className="w-auto h-14"
+        />
+      </button>
+    </header>
+  );
+};
 
 export default Header;
