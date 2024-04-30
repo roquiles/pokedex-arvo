bed4061e89012e26e24f8481885b7f0729796887
Roberta Quiles <roberta@MacBook-Pro-de-Roberta.local>
2024-04-29 23:15:21 -0300

    Style adjustments

--- src/pages/components/Footer/index.tsx
+++ src/pages/components/Footer/index.tsx
@@ -1,12 +1,12 @@
 import Link from 'next/link';
 
 const Footer: React.FC = () => (
-  <footer className="w-full h-16 text-center text-gray-700 bottom-0 absolute inset-x-0">
+  <footer className="w-full flex justify-center items-center min-h-24 text-gray-700 bottom-0 inset-x-0">
     {new Date().getFullYear()} - Created by
     <Link
       href="https://github.com/roquiles"
       target="_blank"
-      className="text-violet-800 hover:text-violet-500"
+      className="text-violet-800 hover:text-violet-500 "
     >
       &nbsp;roquiles
     </Link>
--- src/pages/components/Layout/index.tsx
+++ src/pages/components/Layout/index.tsx
@@ -7,11 +7,11 @@ type LayoutProps = {
 };
 
 const Layout: React.FC<LayoutProps> = ({ children }) => (
-  <>
+  <div className="flex flex-col h-full">
     <Header />
     {children}
     <Footer />
-  </>
+  </div>
 );
 
 export default Layout;
