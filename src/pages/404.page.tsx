0afae3884c9cafd14084545fce2c277bdb1deb5c
Roberta Quiles <roberta@MacBook-Pro-de-Roberta.local>
2024-05-01 12:54:45 -0300

    Add button to return to home

--- src/pages/404.page.tsx
+++ src/pages/404.page.tsx
@@ -1,6 +1,14 @@
+import Link from 'next/link';
+
 const Custom404: React.FC = () => (
-  <div className="h-full flex justify-center items-center">
-    <h1 className="text-xl">404 - Page Not Found</h1>
+  <div className="h-full flex flex-col justify-center items-center">
+    <h1 className="text-xl mb-4">404 - Page Not Found</h1>
+    <Link
+      href="/"
+      className="font-bold text-indigo-500 hover:underline decoration-indigo-500"
+    >
+      Go back to Home
+    </Link>
   </div>
 );
 
